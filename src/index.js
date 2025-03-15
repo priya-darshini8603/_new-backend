e2cdaf759afe455b041e86976ffd610ccc99c369
pavani <paaani@2004@gmail.com>
2025-03-15 19:04:54 +0530

    folder structures chnages in public

--- src/index.js
+++ src/index.js
@@ -1,27 +1,41 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+
 const http=require('http')
 const path = require('path');
 const hbs = require('hbs');
 const cors = require('cors');
+
 const bodyParser = require('body-parser');
 const mongoose = require('./mongodb');
+
 const authRoutes = require('./routes/authRoutes');
 const paymentRoutes = require('./routes/paymentRoutes');
-const socketIo = require('socket.io');
+
 const trackerRoutes = require('./routes/trackerRoutes');
+const templatepath = path.join('template');
+const server = http.createServer(app);
+//praser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+//static files location
+app.use(express.static(path.join(__dirname, 'public')));
+
+//view engine
+app.set('view engine', 'hbs');
+app.set('views', templatepath);
 
 
 
 
 
 const config = require('./config/razorpay');
-const templatepath = path.join('template');
-const server = http.createServer(app);
+
 const io = socketIo(server);
-app.use(express.static('public'));
-app.use(express.static('static'));
+//app.use(express.static('public'));
+//app.use(express.static('static'));
 app.use(bodyParser.json());
 app.use('/api/location',trackerRoutes);
 app.get('/bus-incharge/:page', (req, res) => res.render(`bus-incharge/${req.params.page}`));
@@ -31,21 +45,10 @@ app.use('/js', express.static('src'));
 
 
 // Export io to use in other files
-module.exports.io = io;
-io.on('connection', (socket) => {
-  console.log('New client connected');
-  
-});
 
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.set('view engine', 'hbs');
-//app.set('view engine','js')
-app.set('views', templatepath);
+
 
 
 app.use(authRoutes);
