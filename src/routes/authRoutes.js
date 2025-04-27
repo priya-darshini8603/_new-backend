11823fedc5cd9ccf5481a1b5ee2c7d40574328e7
pavani <paaani@2004@gmail.com>
2025-04-27 10:48:36 +0530

    login has changed

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const authController = require("../controllers/authcontrollers");
 
 router.get("[/,home]", (req, res) => res.render("home"));
-router.get("/login", (req, res) => res.render("login"));
+router.get("/loginform", (req, res) => res.render("loginform"));
 router.get("/signup", (req, res) => res.render("signup"));
 router.get("/routelist", (req, res) => res.render("routelist"));
 router.get("/contact", (req, res) => res.render("contact"));
@@ -11,7 +11,7 @@ router.get("/about", (req, res) => res.render("about"));
 router.get("/gpstracker", (req, res) => res.render("gpstracker"));
 router.get("/otp", (req, res) => res.render("otp"));
 router.get("/resetpassword", (req, res) => res.render("resetpassword"));
-//router.get("/student", (req, res) => res.render("student"));
+router.get("/forgpass", (req, res) => res.render("forgpass"));
 
 
 
